c003b16d3a4e9f175fe6a4c4f663106859c8851d
Masood ul Rehman <masooddulrehman@gmail.com>
2024-08-19 15:23:52 +0500

    added shadcn

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between px-10 py-5">
+      <h4>Web3 Wallet</h4>
+    </div>
+  );
+};
+
+export default Header;
