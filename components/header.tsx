30bcb4dc4e2753c673120a48298fd5ce71f65046
Masood ul Rehman <masooddulrehman@gmail.com>
2024-08-19 15:28:11 +0500

    added clerk auth

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 const Header = () => {
   return (
     <div className="flex justify-between px-10 py-5">
       <h4>Web3 Wallet</h4>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </div>
   );
 };
